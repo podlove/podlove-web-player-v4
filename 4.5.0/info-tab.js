aa23d3c805d8e3ac63b4785dcf9e4185aaa9c69a
Alexander Heimbuch <alexander-heimbuch@users.noreply.github.com>
2019-05-04 13:39:52 +0200

    github-pages publish.

--- 4.5.0/info-tab.js
+++ 4.5.0/info-tab.js
@@ -1,2 +1,2 @@
-// [AIV_SHORT]  Build version: 4.5.0 - Saturday, May 4th, 2019, 11:18:48 AM  
+// [AIV_SHORT]  Build version: 4.5.0 - Saturday, May 4th, 2019, 11:39:03 AM  
  (window.webpackJsonp=window.webpackJsonp||[]).push([[7],{261:function(n,w,e){"use strict";e.r(w),w.default=e(245).default}}]); 
