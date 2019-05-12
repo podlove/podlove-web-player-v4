a174bf2c78f3844b777e4adccec104be4dcb9651
Alexander Heimbuch <alexander-heimbuch@users.noreply.github.com>
2019-05-12 21:19:45 +0200

    github-pages publish.

--- 4.5.2/chapters-tab.js
+++
@@ -1,2 +0,0 @@
-// [AIV_SHORT]  Build version: 4.5.2 - Sunday, May 5th, 2019, 7:27:02 PM  
- (window.webpackJsonp=window.webpackJsonp||[]).push([[5],{264:function(n,w,e){"use strict";e.r(w),w.default=e(250).default}}]); 
