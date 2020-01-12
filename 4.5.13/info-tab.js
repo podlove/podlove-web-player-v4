12474031ea743fffd2100363dc5846204cdc84c2
Alexander Heimbuch <alexander-heimbuch@users.noreply.github.com>
2020-01-12 09:37:13 +0100

    github-pages publish.

--- 4.5.13/info-tab.js
+++ 4.5.13/info-tab.js
@@ -1,2 +1,2 @@
-// [AIV_SHORT]  Build version: 4.5.12 - Tuesday, December 24th, 2019, 7:57:54 AM  
+// [AIV_SHORT]  Build version: 4.5.12 - Sunday, January 12th, 2020, 8:36:30 AM  
  (window.webpackJsonp=window.webpackJsonp||[]).push([[7],{276:function(n,w,e){"use strict";e.r(w),w.default=e(257).default}}]); 
